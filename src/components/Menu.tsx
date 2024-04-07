e975821a663b15e0ed7cbfc7005ff4207c8763d0
walid <cha_wal100@yahoo.fr>
2024-04-08 00:54:51 +0200

    update menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -15,20 +15,23 @@ const user=false
 const Menu = () => {
   const [open, setOpen] = useState(false);
   return (
-    <div>
+  <div>
         {!open? (<Image src="/open.png" alt='' width={20} height={20} onClick={()=>setOpen(true)}/>)
         :(<Image src="/close.png" alt='' width={20} height={20} onClick={()=>setOpen(false)}/>
         )}
-      <div className='bg-red-500 text-white absolute left-0 top-24 w-full h-[calc(100vh-6rem)] flex flex-col gap-8 items-center justify-center text-xl z-10'>
+    {open && <div className='bg-red-500 text-white absolute left-0 top-24 w-full h-[calc(100vh-6rem)] flex flex-col gap-8 items-center justify-center text-xl z-10'>
      {links.map((item)=>(
-        <Link href={item.url} key={item.id}>{item.title}</Link>
+        <Link href={item.url} key={item.id} onClick={()=>setOpen(false)}>{item.title}</Link>
      ))}
-    {!user?(<Link href="/login">Login</Link>
+    {!user?(<Link href="/login" onClick={()=>setOpen(false)}>Login</Link>
     ):(
-    <Link href="/orders">Orders</Link>
+    <Link href="/orders" onClick={()=>setOpen(false)}>Orders</Link>
     )}
+    <Link href="/cart" onClick={()=>setOpen(false)}>
     <Carticon/>
-    </div>  
+    </Link>
+    
+    </div>}  
     </div>
   )
 }
