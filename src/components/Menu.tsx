0c3742bfa859493ec55046fc3031c234a922f79f
walid <cha_wal100@yahoo.fr>
2024-04-07 22:47:15 +0200

    update menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,15 +1,28 @@
 "use client"
 import Image from 'next/image'
+import Link from 'next/link'
 import React, { useState } from 'react'
 
+const links =[
+    {id:1, title: "Homepage", url:"/"},
+    {id:1, title: "Menu", url:"/menu"},
+    {id:1, title: "Workink Hours", url:"/"},
+    {id:1, title: "Contact", url:"/"},
+]
 const Menu = () => {
   const [open, setOpen] = useState(false);
   return (
     <div>
         {!open? (<Image src="/open.png" alt='' width={20} height={20} onClick={()=>setOpen(true)}/>)
-        :(<Image src="/close.png" alt='' width={20} height={20} onClick={()=>setOpen(false)}/>)}
-        
+        :(<Image src="/close.png" alt='' width={20} height={20} onClick={()=>setOpen(false)}/>
+        )}
+      <div>
+     {links.map((item)=>(
+        <Link href={item.url} key={item.id}>{item.title}</Link>
+     ))}
+      </div>  
     </div>
+   
   )
 }
 
