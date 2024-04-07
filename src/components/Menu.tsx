e98bfca2bb97e13331b6f756ab196cfc2afcd45f
walid <cha_wal100@yahoo.fr>
2024-04-07 22:20:02 +0200

    update menu, navbar

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,10 +1,14 @@
+"use client"
 import Image from 'next/image'
-import React from 'react'
+import React, { useState } from 'react'
 
 const Menu = () => {
+  const [open, setOpen] = useState(false);
   return (
     <div>
-        <Image src="/open.png" alt='' width={20} height={20}/>
+        {!open? (<Image src="/open.png" alt='' width={20} height={20} onClick={()=>setOpen(true)}/>)
+        :(<Image src="/close.png" alt='' width={20} height={20} onClick={()=>setOpen(false)}/>)}
+        
     </div>
   )
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,9 +4,9 @@ import Menu from './Menu'
 
 const Navbar = () => {
   return (
-    <div className='h-12 text-red-500 p-4 flex justify-between'>
+    <div className='h-12 text-red-500 p-4 flex justify-between border-b-2 border-b-red-500 uppercase'>
       {/*LOGO*/}
-      <div>
+      <div className='text-xl'>
         <Link href="/">Massimo</Link>
       </div>
       {/*MOBILE MENU*/}
