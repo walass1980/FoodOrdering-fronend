85e39a0e69ce5e0d1bce947107e455afc0d16b64
walid <cha_wal100@yahoo.fr>
2024-04-09 02:24:26 +0200

    adding slider component

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,8 +1,19 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Slider = () => {
   return (
-    <div>Slider</div>
+    <div className='flex flex-col h-screen'>
+        {/*TEXT CONTAINER*/}
+        <div className='h-1/2'>
+            <h1 className=''>Test</h1>
+            <button className=''>Order Now</button>
+        </div>
+        {/*IMAGE CONTAINER*/}
+        <div className='w-full h-1/2 relative'>
+            <Image src="/slide1.png" alt="" fill/>
+        </div>
+    </div>
   )
 }
 
