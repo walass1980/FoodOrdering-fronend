8e6dc3977f018dfc4be829e593c50beac94c8317
walid <cha_wal100@yahoo.fr>
2024-04-10 04:39:22 +0200

    update slider

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -32,7 +32,7 @@ const Slider = () => {
 //  },[])
 
   return (
-    <div className='flex flex-col h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)]'>
+    <div className='flex flex-col h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] lg:flex-row'>
         {/*TEXT CONTAINER*/}
         <div className='h-1/2 flex items-center justify-center flex-col gap-8 text-red-500 font-bold'>
             <h1 className='text-center text-5xl uppercase p-4 md:p-10 md:6xl lg:7xl'>
