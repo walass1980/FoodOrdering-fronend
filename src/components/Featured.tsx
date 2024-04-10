2139a4b0258fc18e8bf658a7fce62f96e10e5376
walid <cha_wal100@yahoo.fr>
2024-04-10 05:23:55 +0200

    featured modified

--- src/components/Featured.tsx
+++ src/components/Featured.tsx
@@ -7,17 +7,17 @@ const Featured = () => {
       {/*WRAPPER*/}
       <div className='w-max flex'>
       {/*SINGLE ITEMS*/}
-        <div className='w-screen h-[60vh] flex flex-col items-center justify-around'>
+        <div className='w-screen h-[60vh] flex flex-col items-center justify-around p-4'>
       {/*IMAGE CONTAINER*/}
       <div className='relative flex-1 w-full'>
-        <Image src="/temporary/p1.png" alt='' fill/>
+        <Image src="/temporary/p1.png" alt='' fill className='object-contain'/>
       </div>
       {/*TEXT CONTAINER*/}
-      <div className='flex-1'>
-        <h1>Title</h1>
+      <div className='flex-1 flex flex-col gap-4'>
+        <h1 className='text-xl font-bold uppercase'>Title</h1>
         <p>Desc</p>
         <span>123</span>
-        <button>Add to Cart</button>
+        <button className='bg-red-500 text-white p-2 rounded-md'>Add to Cart</button>
       </div>
 
         </div>
