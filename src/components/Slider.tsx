c34b7d8d98e5990593dbe28795b2480e38ffef25
walid <cha_wal100@yahoo.fr>
2024-04-10 04:32:34 +0200

    slider modified

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -22,19 +22,20 @@ const data = [
 const Slider = () => {
   const [currentSlide, setcurrentSlide] = useState(0);
   
-useEffect(()=>{
-   const interval = setInterval(
-     ()=>setcurrentSlide((prev) => (prev ===data.length-1 ? 0 : prev+1)),
-   4000
-)
-   return ()=>clearInterval(interval)
-  },[])
+//useEffect(()=>{
+ //  const interval = setInterval(
+ //    ()=>
+ //       setcurrentSlide((prev) => (prev ===data.length-1 ? 0 : prev+1)),
+ //  4000
+//)
+//   return ()=>clearInterval(interval)
+//  },[])
 
   return (
     <div className='flex flex-col h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)]'>
         {/*TEXT CONTAINER*/}
         <div className='h-1/2 flex items-center justify-center flex-col gap-8 text-red-500 font-bold'>
-            <h1 className='text-center text-5xl uppercase p-4 md:6xl lg:7xl'>
+            <h1 className='text-center text-5xl uppercase p-4 md:p-10 md:6xl lg:7xl'>
                 {data[currentSlide].title}
             </h1>
             <button className='bg-red-500 text-white px-8 py-4 rounded-md'>Order Now</button>
