d5df1eb8ab49d891360cd1425d6b1fb8c19ab799
Walid <144150066+walass1980@users.noreply.github.com>
2024-04-11 02:14:54 +0000

    offer updated

--- src/components/Offer.tsx
+++ src/components/Offer.tsx
@@ -4,7 +4,7 @@ import CountDown from './CountDown'
 
 const Offer = () => {
   return (
-    <div className='bg-black h-screen flex flex-col md:flex-row'>
+    <div className="bg-black h-screen flex flex-col md:flex-row md:justify-between md:bg-[url('/offerBg.png')] md:h-[70vh] ">
       {/*TEXT CONTAINER*/}
       <div className='flex-1 flex flex-col items-center justify-center text-center gap-8 p-6'>
        <h1 className='text-white text-5xl font-bold xl:text-6xl'>Delicious Burger & French Fry</h1>
@@ -14,7 +14,7 @@ const Offer = () => {
        <button className='bg-red-500 text-white py-3 px-6 rounded-md'>Order Now</button>
       </div>
       {/*IMAGE CONTAINER*/}
-      <div className='relative w-full flex-1'>
+      <div className='relative w-full flex-1 md:h-full'>
       <Image src="/offerProduct.png" alt="" fill className='object-contain' />
       </div>
     </div>
