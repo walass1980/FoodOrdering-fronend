4daf3a10092a02d27e9db7ed0f69b578a00ca1cc
walid <cha_wal100@yahoo.fr>
2024-04-11 21:11:38 +0200

    menupage update

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -4,19 +4,18 @@ import React from 'react'
 
 const MenuPage= () => {
   return (
-    <div className='p-4 lg:p-40 h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex flex-col md:flex-row items-center'>
+    <div className='p-4 lg:px-20 xl:px-40 h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex flex-col md:flex-row items-center'>
     {menu.map(category=>(
-     <Link href={category.slug} 
+     <Link href={`/menu/${category.slug}`} 
      key={category.id} 
-     className='' 
-     style={{backgroundImage:`url(${category.img})`}}>
-      <div className=''>
-        <div className=''>
-          <h1>{category.title}</h1>
-          <p>{category.desc}</p>
-          <button className=''>Explore</button>
+     className='w-full h-1/3 bg-cover p-8 md:h-1/2' 
+     style={{backgroundImage:`url(${category.img})`}}
+     >
+        <div className={`text-${category.color} w-1/2`}>
+          <h1 className='uppercase font-bold text-3xl'>{category.title}</h1>
+          <p className='text-sm py-8'>{category.desc}</p>
+          <button className={`hidden 2xl:block bg-${category.color} py-2 px-4 rounded-md text-${category.color==="black" ? "white" : "bg-red-500"}`}>Explore</button>
         </div>
-      </div>
      </Link>
     ))}
     </div>
