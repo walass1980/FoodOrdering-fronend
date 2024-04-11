2eaca6b58a31e139a2fe58c36d601484d4049862
walid <cha_wal100@yahoo.fr>
2024-04-12 00:53:45 +0200

    productpage updated

--- src/app/menu/[category]/page.tsx
+++ src/app/menu/[category]/page.tsx
@@ -7,7 +7,7 @@ const CategoryPage = () => {
   return (
     <div className='flex flex-wrap text-red-500'>
       {pizzas.map(item=>(
-        <Link className='flex flex-col justify-between group w-full h-[60vh] p-4 border-r-2 border-b-2 border-red-500 sm:w-1/2 lg:w-1/3' href={`/product/${item.id}`} key={item.id}>
+        <Link className='flex flex-col justify-between group w-full h-[60vh] p-4 border-r-2 border-b-2 border-red-500 sm:w-1/2 lg:w-1/3 even:bg-fuchsia-50' href={`/product/${item.id}`} key={item.id}>
          {/*IMAGE CONTAINER*/}
          {item.img && (
          <div className='relative h-[80%]'>
@@ -17,7 +17,7 @@ const CategoryPage = () => {
          {/*TEXT CONTAINER*/}
          <div className='flex items-center justify-between font-bold'>
         <h1 className='text-2xl uppercase p-2'>{item.title}</h1>
-        <h2>${item.price}</h2>
+        <h2 className='group-hover:hidden text-xl'>${item.price}</h2>
         <button className='bg-red-500 hidden group-hover:block uppercase text-white p-2 rounded-md'>Add to Cart</button>
          </div>
         </Link>
