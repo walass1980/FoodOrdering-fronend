b61e1875f8762ce945a9064dc9d804df39d7fe3f
Walid <144150066+walass1980@users.noreply.github.com>
2024-04-11 01:29:17 +0000

    offer updated

--- src/components/Offer.tsx
+++ src/components/Offer.tsx
@@ -1,15 +1,18 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Offer = () => {
   return (
-    <div className=''>
+    <div className='bg-black h-screen flex flex-col md:flex-row'>
       {/*TEXT CONTAINER*/}
-      <div className=''>
-
+      <div className='flex-1'>
+       <h1 className=''></h1>
+       <p className=''></p>
+       <button className=''>Order Now</button>
       </div>
       {/*IMAGE CONTAINER*/}
-      <div className=''>
-        
+      <div className='relative w-full flex-1'>
+      <Image src="/offerProduct.png" alt="" fill className='object-contain' />
       </div>
     </div>
   )
