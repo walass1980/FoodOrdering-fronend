d6ac629dd130952fd2b00f17190e557f312b2ff3
walid <cha_wal100@yahoo.fr>
2024-04-12 00:10:01 +0200

    productpage added

--- src/app/menu/[category]/page.tsx
+++ src/app/menu/[category]/page.tsx
@@ -1,8 +1,26 @@
+import { pizzas } from '@/data'
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const CategoryPage = () => {
   return (
-    <div> CategoryPage</div>
+    <div className='flex flex-wrap text-red-500'>
+      {pizzas.map(item=>(
+        <Link className='w-full h-[60vh] border-r-2 border-b-2 border-red-500 sm:w-1/2 lg:w-1/3' href={`/product/${item.id}`} key={item.id}>
+         {/*IMAGE CONTAINER*/}
+         {item.img && (
+         <div className='relative'>
+         <Image src={item.img} alt='' fill/>
+         </div>
+        )}
+         {/*TEXT CONTAINER*/}
+         <div className=''>
+
+         </div>
+        </Link>
+      ))}
+    </div>
   )
 }
 
