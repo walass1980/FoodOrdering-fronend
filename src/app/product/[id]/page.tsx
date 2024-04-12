30170f7a3b3d5d884e97b7b169832f83546a37a4
walid <cha_wal100@yahoo.fr>
2024-04-12 02:15:08 +0200

    pricepage updated

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -6,14 +6,14 @@ import React from 'react'
 const SingleProduct = () => {
   return (
     <div className='p-4 lg:px-20 xl:px-40 flex flex-col h-screen justify-around text-red-500 md:flex-row'>
-     {/*IMAGE CONTAINE*/}
-     {singleProduct.img && (<div className='relative'>
+     {/*IMAGE CONTAINER*/}
+     {singleProduct.img && (<div className='relative w-full h-1/2'>
       <Image src={singleProduct.img} key={singleProduct.id} alt='' className='object-contain' fill/>
      </div>
     )}
      {/*TEXT CONTAINER*/}
-     <div className=''>
-     <h1 className=''>{singleProduct.title}</h1>
+     <div className='h-1/2 flex flex-col gap-4'>
+     <h1 className='text-3xl font-bold uppercase'>{singleProduct.title}</h1>
      <p className=''>{singleProduct.desc}</p>
      <Price id={singleProduct.id} price={singleProduct.price} options={singleProduct.options}/>
      </div>
--- src/components/Price.tsx
+++ src/components/Price.tsx
@@ -7,7 +7,27 @@ type Props = {
 }
 const Price = ({price,id,options}:Props) => {
   return (
-    <div>Price</div>
+    <div>
+    <h2>${price.toFixed(2)}</h2>
+    {/*OPTION CONTAINER*/}
+    <div className=''>
+     {options?.map(option=>(
+     <button className='' key={option.title}>{option.title}</button> 
+     ))}
+    </div>
+    {/*QUANTITY AND ADD BUTTON*/}
+    <div className=''>
+    {/*QUANTIYI*/}
+     <span>Quantity</span>
+     <div>
+        <button>{'<'}</button>
+        <span>1</span>
+        <button>{'>'}</button>
+    {/*CART BUTTON*/}
+    <button className="">Add to Cart</button>
+     </div>
+    </div>
+    </div>
   )
 }
 
