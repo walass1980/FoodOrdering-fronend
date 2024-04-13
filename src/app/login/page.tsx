5e36b84fc41e5c2717041ba850d0e74fbb2907b1
walid <cha_wal100@yahoo.fr>
2024-04-13 08:21:38 +0200

    loginpage updated

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -7,15 +7,15 @@ const LoginPage = () => {
       {/*BOX*/}
       <div className=''>
         {/*IMAGE CONTANER*/}
-        <div className=''>
+        <div className='relative'>
           <Image src="/loginBg.png" alt='' fill/>
         </div>
         {/*FORM CONTAINER*/}
         <div className=''>
           <h1 className=''>Welcome</h1>
           <p className=''>Log into your account or create a new one using social buttons</p>
-          <button className=''>
-            <Image src="/google.png" alt='' width={20} height={20} />
+          <button className='flex gap-4 p-4 ring-1 ring-orange-100 rounded-md'>
+            <Image src="/google.png" alt='' width={20} height={20} className='object-contain'/>
             <span>Sign in with Google</span>
           </button>
         </div>
