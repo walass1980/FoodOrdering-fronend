12e8178372d9bec5b91e6fdcceadc46c67a26e60
walid <cha_wal100@yahoo.fr>
2024-04-13 08:16:04 +0200

    create loginpage

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,8 +1,27 @@
+import Image from 'next/image'
 import React from 'react'
 
 const LoginPage = () => {
   return (
-    <div>LoginPage</div>
+    <div className=''>
+      {/*BOX*/}
+      <div className=''>
+        {/*IMAGE CONTANER*/}
+        <div className=''>
+          <Image src="/loginBg.png" alt='' fill/>
+        </div>
+        {/*FORM CONTAINER*/}
+        <div className=''>
+          <h1 className=''>Welcome</h1>
+          <p className=''>Log into your account or create a new one using social buttons</p>
+          <button className=''>
+            <Image src="/google.png" alt='' width={20} height={20} />
+            <span>Sign in with Google</span>
+          </button>
+        </div>
+      </div>
+
+    </div>
   )
 }
 
