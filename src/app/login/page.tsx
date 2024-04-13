fcf22f4c0973b5a5038f400afb3603e16654b6ea
walid <cha_wal100@yahoo.fr>
2024-04-13 08:28:31 +0200

    liginpage modified

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const LoginPage = () => {
@@ -15,9 +16,14 @@ const LoginPage = () => {
           <h1 className=''>Welcome</h1>
           <p className=''>Log into your account or create a new one using social buttons</p>
           <button className='flex gap-4 p-4 ring-1 ring-orange-100 rounded-md'>
-            <Image src="/google.png" alt='' width={20} height={20} className='object-contain'/>
+            <Image src="/google.png" alt='' width={20} height={20} className="object-contain"/>
             <span>Sign in with Google</span>
           </button>
+          <button className='flex gap-4 p-4 ring-1 ring-blue-100 rounded-md'>
+            <Image src="/facebook.png" alt='' width={20} height={20} className="object-contain"/>
+            <span>Sign in with Facebook</span>
+          </button>
+          <p>Have a problem?<Link href="/">Contact us</Link></p>
         </div>
       </div>
 
