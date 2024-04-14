af7e8a95bf3edce26c20bf370582187c4df8cb08
Walid <144150066+walass1980@users.noreply.github.com>
2024-04-14 00:57:10 +0000

    loginpage updated

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,16 +4,16 @@ import React from 'react'
 
 const LoginPage = () => {
   return (
-    <div className='p-4 h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex items-center justify-center'>
+    <div className='p-4 h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex items-center justify-center bg-yellow-200'>
       {/*BOX*/}
-      <div className='h-full shadow-xl rounded-md flex flex-col md:flex-row'>
+      <div className='h-full shadow-xl rounded-md flex flex-col md:flex-row md:h-[70%] md:w-full bg-green-300 lg:w-[60%] 2xl:w-1/2'>
         {/*IMAGE CONTANER*/}
-        <div className='relative h-1/3 w-full'>
+        <div className='relative h-1/3 w-full md:h-full md:w-1/2'>
           <Image src="/loginBg.png" alt='' fill className='object-cover'/>
         </div>
         {/*FORM CONTAINER*/}
-        <div className='p-10 flex flex-col gap-8'>
-          <h1 className='font-bold text-xl'>Welcome</h1>
+        <div className='p-10 flex flex-col gap-8 md:w-1/2'>
+          <h1 className='font-bold text-xl xl:text-3xl'>Welcome</h1>
           <p className=''>Log into your account or create a new one using social buttons</p>
           <button className='flex gap-4 p-4 ring-1 ring-orange-100 rounded-md'>
             <Image src="/google.png" alt='' width={20} height={20} className="object-contain"/>
@@ -23,8 +23,8 @@ const LoginPage = () => {
             <Image src="/facebook.png" alt='' width={20} height={20} className="object-contain"/>
             <span>Sign in with Facebook</span>
           </button>
-          <p>
-            Have a problem?<Link href="/">Contact us</Link>
+          <p className='text-sm'>
+            Have a problem?<Link className='underline' href="/">Contact us</Link>
           </p>
         </div>
       </div>
